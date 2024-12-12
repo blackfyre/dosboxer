6db1b2fe41a832c401951f05ce445bc2edc3cc46
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-12-12 21:46:14 +0100

    chore: repo inti

---
+++ cmd/dosboxer/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("yahoo")
+}
+
+// check config
+// ask for missing values
+// scan dos games folder
+// identify games
+// cache metadata
+// run with config
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blackfyre/dosboxer
+
+go 1.23.3
